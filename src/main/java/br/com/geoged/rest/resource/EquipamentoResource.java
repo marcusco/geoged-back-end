84d5e4db237af2e5389f70a6b6a0cd83fb8ec816
marcusco <marcuscesaro@gmail.com>
2021-12-20 09:19:17 -0300

    Resource

--- src/main/java/br/com/geoged/rest/resource/EquipamentoResource.java
+++ src/main/java/br/com/geoged/rest/resource/EquipamentoResource.java
@@ -1,5 +1,6 @@
 package br.com.geoged.rest.resource;
 
+import java.util.List;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,37 +14,58 @@ import br.com.geoged.entity.Equipamento;
 import br.com.geoged.exceptions.GeoGedException;
 import br.com.geoged.service.EquipamentoService;
 
+
 @RestController
 @RequestMapping(value = "/equipamento")
-public class EquipamentoResource {
+public class EquipamentoResource
+{
+	@Autowired
+	private EquipamentoService equipamentoService;
+	@GetMapping(value = "/find_by_nome/{nome}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Response findByNome(@QueryParam("tenantId") Integer tenantId, @QueryParam("nome") String nome)
+	{
+		Response response;
+		var tmp = equipamentoService.findByNome(tenantId, nome);
+		if(tmp != null)
+		{
+			response = Response.ok(tmp).build();
+		}
+		else
+		{
+			response = Response.noContent().build();
+		}
+		return response;
+	}
 
-  @Autowired
-  private EquipamentoService equipamentoService;
+	@PostMapping(value = "/salvar")
+	public Response salvar(@RequestBody Equipamento entity) throws GeoGedException
+	{
+		Response response;
+		var tmp = equipamentoService.save(entity);
+		if(tmp != null)
+		{
+			response = Response.ok(tmp).build();
+		}
+		else
+		{
+			response = Response.noContent().build();
+		}
+		return response;
+	}
 
-  @GetMapping(value = "/find_by_nome/{nome}", produces = MediaType.APPLICATION_JSON_VALUE)
-  public Response findByNome(@QueryParam("tenantId") Integer tenantId,
-      @QueryParam("nome") String nome) {
-    Response response;
-    var tmp = equipamentoService.findByNome(tenantId, nome);
-    if (tmp != null) {
-      response = Response.ok(tmp).build();
-    } else {
-      response = Response.noContent().build();
-    }
-    return response;
-  }
-
-  @PostMapping(value = "/salvar")
-  public Response salvar(@RequestBody Equipamento entity) throws GeoGedException {
-    Response response;
-    var tmp = equipamentoService.save(entity);
-    if (tmp != null) {
-      response = Response.ok(tmp).build();
-    } else {
-      response = Response.noContent().build();
-    }
-    return response;
-  }
-
-
+	@GetMapping(value = "/find_by_tenant_id")
+	public Response findByTenantId(@QueryParam(value = "tenantId") Integer tenantId)
+	{
+		Response response;
+		List<Equipamento> tmp = equipamentoService.findByTenantId(tenantId);
+		if(tmp != null)
+		{
+			response = Response.ok(tmp).build();
+		}
+		else
+		{
+			response = Response.noContent().build();
+		}
+		return response;
+	}
 }
