a1bba0788cd4e60807500edfd7bf003349bed2bc
marcusco <marcuscesaro@gmail.com>
2021-12-22 19:09:10 -0300

    VooRespource

--- src/main/java/br/com/geoged/rest/resource/VooResource.java
+++ src/main/java/br/com/geoged/rest/resource/VooResource.java
@@ -1,9 +1,11 @@
 package br.com.geoged.rest.resource;
 
+import java.util.List;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,36 +14,44 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.geoged.entity.Voo;
 import br.com.geoged.exceptions.GeoGedException;
 import br.com.geoged.service.VooService;
+import br.com.geoged.util.CollectionsUtil;
+
 
 @RestController
 @RequestMapping(value = "/voo", produces = MediaType.APPLICATION_JSON_VALUE)
-public class VooResource {
+public class VooResource
+{
+	@Autowired
+	private VooService vooService;
+	@GetMapping(value = "/find_by_tenant_id")
+	public ResponseEntity<List<Voo>> findByTenantId(@QueryParam(value = "tenantId") Integer tenantId)
+	{
+		ResponseEntity<List<Voo>> response;
+		var tmp = vooService.findByTenantId(tenantId);
+		if(!CollectionsUtil.isEmpty(tmp))
+		{
+			response = ResponseEntity.ok(tmp);
+		}
+		else
+		{
+			response = ResponseEntity.notFound().build();
+		}
+		return response;
+	}
 
-  @Autowired
-  private VooService vooService;
-
-  @GetMapping(value = "/find_all")
-  public Response findByTenantId(@QueryParam(value = "tenantId") Integer tenantId) {
-    Response response;
-    var tmp = vooService.findByTenantId(tenantId);
-    if (tmp != null) {
-      response = Response.ok(tmp).build();
-    } else {
-      response = Response.noContent().build();
-    }
-    return response;
-  }
-
-  @PostMapping(value = "/salvar")
-  public Response salvar(@RequestBody Voo entity) throws GeoGedException {
-    Response response;
-    var tmp = vooService.save(entity);
-    if (tmp != null) {
-      response = Response.ok(tmp).build();
-    } else {
-      response = Response.noContent().build();
-    }
-    return response;
-  }
-
+	@PostMapping(value = "/salvar")
+	public Response salvar(@RequestBody Voo entity) throws GeoGedException
+	{
+		Response response;
+		var tmp = vooService.save(entity);
+		if(tmp != null)
+		{
+			response = Response.ok(tmp).build();
+		}
+		else
+		{
+			response = Response.noContent().build();
+		}
+		return response;
+	}
 }
