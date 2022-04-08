1f6cc6b5d52a813a1d26609ef008903f15b87675
marcusco <marcuscesaro@gmail.com>
2022-04-08 17:30:13 -0300

    Messages
    Signed-off-by: marcusco <marcuscesaro@gmail.com>

--- src/main/java/br/com/geoged/controller/MessagesController.java
+++ src/main/java/br/com/geoged/controller/MessagesController.java
@@ -2,13 +2,18 @@ package br.com.geoged.controller;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 public class MessagesController
 {
+	
 	 @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Greeting greeting(Message message) throws Exception {
-        Thread.sleep(1000);
+		 //Thread.sleep(5000);
         return new Greeting(message.getName());
     }
+	 
 }
